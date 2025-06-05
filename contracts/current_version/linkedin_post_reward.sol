d03bed4e07eb2401ba9e806db5b88719bfe51512
theta01adi <aditya.patel@thetatechnolabs.com>
2025-06-05 19:12:43 +0530

    User post submit reward functionality : _rewardUser() add, REWARD variable set

--- contracts/current_version/linkedin_post_reward.sol
+++ contracts/current_version/linkedin_post_reward.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-
-contract LinkedInPostReward is Ownable2Step {
-
+contract LinkedInPostReward is Ownable2Step , ReentrancyGuard {
     // error NotRegistered();
 
     address[] private submitters;
@@ -14,69 +13,119 @@ contract LinkedInPostReward is Ownable2Step {
         address submitter;
         string cid;
     }
-    
-    mapping (address => string) public userToName;
-    mapping (address => Submission) private postCid;
 
-    event UserRegistered(address indexed user, uint createdAt);
+    uint256 constant POST_SUBMIT_REWARD = 0.001 ether;
+    uint256 constant POST_CONTENT_REWARD = 0.1 ether;
+
+    mapping(address => string) public userToName;
+    mapping(address => Submission) private postCid;
+
+    event UserRegistered(address indexed user, uint256 createdAt);
     event PostCidSubmitted(address indexed submitter, string postCid);
+    event UserRewarded(
+        address indexed user,
+        uint256 rewardAmount,
+        string rewardFor
+    );
 
-    constructor() Ownable(msg.sender) {
-
-    }
+    constructor() Ownable(msg.sender) {}
 
     modifier isRegistered(address user) {
-        require(bytes(userToName[user]).length != 0,"You are not registered !!");
+        require(
+            bytes(userToName[user]).length != 0,
+            "You are not registered !!"
+        );
         _;
     }
 
     function isPostSubmitted(address user) public view returns (bool) {
-    return bytes(userToName[user]).length > 0;
+        return bytes(postCid[user].cid).length > 0;
     }
 
-    function isUserRegistered() external view returns(bool) {
-        return bytes(userToName[msg.sender]).length == 0;
-    } 
+    function isUserRegistered() external view returns (bool) {
+        return bytes(userToName[msg.sender]).length != 0;
+    }
 
     // filtering of username on python/react from the url
-    function register_user(address user, string calldata username) external onlyOwner {
+    function register_user(address user, string calldata username)
+        external
+        onlyOwner
+    {
         require(user != address(0), "User address can't be zero!!");
-        require(bytes(userToName[user]).length == 0,"User already registered !!");
-        require(bytes(username).length != 0, "Invalid username lenght !!");
+        require(
+            bytes(userToName[user]).length == 0,
+            "User already registered !!"
+        );
+        require(bytes(username).length != 0, "Invalid username length !!");
 
         userToName[user] = username;
-
         emit UserRegistered(user, block.timestamp);
     }
 
-    function submit_cid(address user, string calldata _postCid) external isRegistered(msg.sender) {
+    function submit_cid(address user, string calldata _postCid)
+        external
+        isRegistered(user)
+        nonReentrant
+    {
         address submitter = user;
-        require( bytes(postCid[submitter].cid).length == 0, "You have already submitted the post !!");
-        require( bytes(_postCid).length != 0, "Cid length can't be zero !!" );
+        require(submitter == _msgSender() || owner() == _msgSender(), "Not authorized !!");
+        require(
+            bytes(postCid[submitter].cid).length == 0,
+            "You have already submitted the post !!"
+        );
+        require(bytes(_postCid).length != 0, "Cid length can't be zero !!");
 
         postCid[submitter].cid = _postCid;
         postCid[submitter].submitter = submitter;
         submitters.push(submitter);
-        
-        emit PostCidSubmitted( submitter ,_postCid);
+
+        emit PostCidSubmitted(submitter, _postCid);
+
+        _rewardUser(user, POST_SUBMIT_REWARD);
+        emit UserRewarded(user, POST_SUBMIT_REWARD, "Post submit reward.");
     }
 
-    function getPostCid(address user) external isRegistered(user) view returns( string memory cid){
+    function _rewardUser(address user, uint256 rewardAmount) internal {
+        require(
+            address(this).balance >= rewardAmount,
+            "Insufficient contract balance !!"
+        );
+
+        (bool success, ) = payable(user).call{value: rewardAmount}("");
+
+        require(success, "Post reward failed !!");
+    }
+
+    function getPostCid(address user)
+        external
+        view
+        isRegistered(user)
+        returns (string memory cid)
+    {
         address caller = msg.sender;
-        require( caller == user || caller == owner(),"Not authorized !!");
-        require( bytes(postCid[user].cid).length != 0, "You don't have submitted the cid !!");
+        require(caller == user || caller == owner(), "Not authorized !!");
+        require(
+            bytes(postCid[user].cid).length != 0,
+            "You don't have submitted the cid !!"
+        );
 
         return postCid[user].cid;
     }
 
-    function getSubmittedCids() external view onlyOwner returns(Submission[] memory submits){
-        submits = new Submission[](submitters.length);  
-        for (uint i=0; i<submitters.length; i++) 
-        {
-            submits[i] =  postCid[submitters[i]] ;
+    function getSubmittedCids()
+        external
+        view
+        onlyOwner
+        returns (Submission[] memory submits)
+    {
+        submits = new Submission[](submitters.length);
+        for (uint256 i = 0; i < submitters.length; i++) {
+            submits[i] = postCid[submitters[i]];
         }
 
         return submits;
     }
 
+    receive() external payable {
+    }
 }
